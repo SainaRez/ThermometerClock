e258145c86793fcf41fc27680357edc9626dc311
Rezvani <srezvani@wpi.edu>
2017-10-03 01:00:05 -0400

    Made fixes

--- main.c
+++ main.c
@@ -51,17 +51,23 @@ __interrupt void ADC12_ISR()
 }
 
 
-
 // Main
 void main(void)
 {
+    WDTCTL = WDTPW | WDTHOLD;       // Stop watchdog timer
+       //_BIS_SR(GIE);
+
     volatile float temperatureDegF;
     volatile float temperatureDegC;
     volatile float degC_per_bit;
     volatile unsigned int bits30, bits85;
     degC_per_bit = ((float)(85.0 - 30.0))/((float)(bits85-bits30));
 
-    while(1) {
+    Graphics_clearDisplay(&g_sContext);
+    Graphics_drawStringCentered(&g_sContext, "Rain Rain", AUTO_STRING_LENGTH, 38, 35, TRANSPARENT_TEXT);
+    Graphics_drawStringCentered(&g_sContext, "Go Away", AUTO_STRING_LENGTH, 38, 55, TRANSPARENT_TEXT);
+    Graphics_flushBuffer(&g_sContext);
+  /*  while(1) {
         if (checkADC == 1) {
             tempArray[circIndex] = currentTemp;
             scrollArray[circIndex] = swVolt;
@@ -103,85 +109,14 @@ void main(void)
 
     WDTCTL = WDTPW | WDTHOLD;       // Stop watchdog timer
 
-    // Some utterly useless instructions -- Step through them
-    // What size does the Code Composer MSP430 Compiler use for the
-    // following variable types? A float, an int, a long integer and a char?
-    flt = flt*tst;
-    X = tst+X;
-    tst = tst-myGrade;    // A number minus a letter?? What's actually going on here?
-                          // What value stored in myGrade (i.e. what's the ASCII code for "A")?
-                          // Thus, what is the new value of tst? Explain?
-
-    // Useful code starts here
     initLeds();
-
-    configDisplay();
-    configKeypad();
-
-    // *** Intro Screen ***
-    Graphics_clearDisplay(&g_sContext); // Clear the display
-
-    // Write some text to the display
-    Graphics_drawStringCentered(&g_sContext, "Welcome", AUTO_STRING_LENGTH, 48, 15, TRANSPARENT_TEXT);
-    Graphics_drawStringCentered(&g_sContext, "to", AUTO_STRING_LENGTH, 48, 25, TRANSPARENT_TEXT);
-    Graphics_drawStringCentered(&g_sContext, "Lab3", AUTO_STRING_LENGTH, 48, 35, TRANSPARENT_TEXT);
-
-    // Draw a box around everything
-    Graphics_Rectangle box = {.xMin = 5, .xMax = 91, .yMin = 5, .yMax = 91 };
-    Graphics_drawRectangle(&g_sContext, &box);
-
-    // We are now done writing to the display.  However, if we stopped here, we would not
-    // see any changes on the actual LCD.  This is because we need to send our changes
-    // to the LCD, which refreshes the display.
-    // Since this is a slow operation, it is best to refresh (or "flush") only after
-    // we are done drawing everything we need.
-    Graphics_flushBuffer(&g_sContext);
-
-    dispThree[0] = ' ';
-    dispThree[2] = ' ';
-
-    while (1)    // Forever loop
-    {
-        // Check if any keys have been pressed on the 3x4 keypad
-        currKey = getKey();
-        if ((currKey >= '0') && (currKey <= '9'))
-            setLeds(currKey - 0x30);
-        if (currKey == '*')
-            BuzzerOn();
-        if (currKey == '#')
-            BuzzerOff();
-
-        if (currKey)
-        {
-            dispThree[1] = currKey;
-            // Draw the new character to the display
-            Graphics_drawStringCentered(&g_sContext, dispThree, dispSz, 48, 55, OPAQUE_TEXT);
-
-            // Refresh the display so it shows the new data
-            Graphics_flushBuffer(&g_sContext);
-
-            // wait awhile before clearing LEDs
-            swDelay(1);
-            setLeds(0);
-        }
-
-
-    }  // end while (1)
+    configDisplay();*/
 }
 
 
 void swDelay(char numLoops)
 {
-	// This function is a software delay. It performs
-	// useless loops to waste a bit of time
-	//
-	// Input: numLoops = number of delay loops to execute
-	// Output: none
-	//
-	// smj, ECE2049, 25 Aug 2013
-
-	volatile unsigned int i,j;	// volatile to prevent optimization
-			                            // by compiler
+	volatile unsigned int i,j;	// volatile to prevent optimization			                            // by compiler
 
 	for (j=0; j<numLoops; j++)
     {
